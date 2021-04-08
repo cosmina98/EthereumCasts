78a897c7b02e2dc4ce102aea97dbe9c35c2a4047
Cosmina98 <34158786+cosmina98@users.noreply.github.com>
2021-04-08 17:15:49 +0100

    Update Lottery.sol

--- lottery/contracts/Lottery.sol
+++ lottery/contracts/Lottery.sol
@@ -1,35 +1,51 @@
-pragma solidity ^0.4.17;
+pragma solidity >=0.7.0 <0.9.0;
 
-contract Lottery {
+contract  Lottery{
+    
     address public manager;
-    address[] public players;
-
-    function Lottery() public {
-        manager = msg.sender;
+    address [] public players;
+    
+    function Lottery1() public{
+        manager=msg.sender;
+       
     }
-
-    function enter() public payable {
-        require(msg.value > .01 ether);
-
-        players.push(msg.sender);
+    
+    function enter() public payable{
+        require(msg.value> .01 ether);
+        
+        (players).push(msg.sender);
+        
     }
-
-    function random() private view returns (uint) {
-        return uint(keccak256(block.difficulty, now, players));
+    
+    
+    function random() public view returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(block.difficulty, block.timestamp,players)));
+    
     }
+    
+    
+    function  pickAWinner() public restricted {
+            uint index = random() % players.length;
+            payable(players[index]).transfer(address(this).balance);
 
-    function pickWinner() public restricted {
-        uint index = random() % players.length;
-        players[index].transfer(this.balance);
-        players = new address[](0);
+            players = new address[](0);
+        
+        
     }
+    
+    modifier restricted(){
+     require (msg.sender == manager);
+   // require(address(this).balance>= 1 ether);
+     _;
 
-    modifier restricted() {
-        require(msg.sender == manager);
-        _;
     }
-
-    function getPlayers() public view returns (address[]) {
+    
+    function getPlayers() public view returns (address[] memory){
         return players;
+        
+        
     }
+   // function pickWinner() public winner returns 
+    //{}
+    
 }
